2af648ea619abdaa6d41c7eb67e5aee29ac0dec4
Gayatri Suslade <gayatrisuslade@outlook.com>
2020-11-06 15:40:01 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef,HostBinding,HostListener,Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Project4';
+  title = 'Working With Host Listeners';
+  constructor(private el:ElementRef,private renderer:Renderer2){
+
+  }
+@HostBinding('style.backgroundColor') background:string;
+
+
+
+@HostListener('click') onclick()
+{
+  alert('Clicked');
+}
+@HostListener('mouseover')onmousemove(eventData:Event){
+   console.log("Mouseover");
+   this.changebgcolor("red");
+   //this.background("yellow");
+  
+
+}
+@HostListener('mouseleave' ) onmouseleave(){
+  console.log("Mouseleave");
+  this.changebgcolor("blue");
+}
+changebgcolor(color:string){
+  this.renderer.setStyle(this.el.nativeElement,'color',color);
+}
 }
